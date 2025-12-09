a7888f36b5a48ff280b38576c3f1f984f2efd153
sayangku-123 <muhammadfhm2004@gmail.com>
2025-12-09 20:13:37 +0800

    Update script.js

--- script.js
+++ script.js
@@ -587,10 +587,10 @@ scene.add(planet);
 
 //Ganti Deskripsi Planet
 const ringTexts = [
-  'Kamalia Putri',//untuk deskripsi planet layer 1
-  "I Love You",//untuk deskripsi planet layer 2
+  '♡Kamalia Putri♡',//untuk deskripsi planet layer 1
+  "♡I Love You♡",//untuk deskripsi planet layer 2
   "♡Happy Birth Day♡",//untuk deskripsi planet layer 3
-  "10/12/2025",//untuk deskripsi planet layer 4
+  "♡10/12/2025♡",//untuk deskripsi planet layer 4
   ...(window.dataCCD && window.dataCCD.data.ringTexts ? window.dataCCD.data.ringTexts : [])
 ];
 
